01ba36c5aafefa0e5c630a05b8c4b5376403d4bb
kosukesuzuki <k.suzuki.sys22@morijyobi.ac.jp>
2023-11-20 11:56:34 +0900

    管理者登録、パスワード変更、ログイン処理の作成

--- 管理者/app.py
+++ 管理者/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import db, string, random
+import db, string, random, mail
 
 app = Flask(__name__)
+app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,15 +15,23 @@ def index():
         # register_exe() から redirect された場合
         return render_template('index.html', msg=msg)
 
+@app.route('/otpmail')
+def otp_mail():
+    return render_template('OTP_mail.html')
+
 @app.route('/', methods=['POST'])
 def login():
     admin_id = request.form.get('adminid')
     password = request.form.get('password')
-    
+    otp = db.otp_login(admin_id)
     # ログイン判定
+    
+    if otp and otp[5] == False:
+        return redirect(url_for('otp_mail'))
+    
     if db.login(admin_id, password):
         session['user'] = True
-        return redirect(url_for('menu'))
+        return redirect(url_for('admintop'))
     
     else:
         error = 'ユーザ名またはパスワードが違います。'
@@ -35,6 +44,71 @@ def login():
 def logout():
     session.pop('user', None)   # sessionの破棄
     return redirect(url_for('index'))   # ログイン画面にリダイレクト    
+
+@app.route('/admintop', methods=['GET'])
+def admintop():
+    if 'user' in session:
+        return render_template('menu.html')   
+    else :
+        return redirect(url_for('index'))   
+    
+@app.route('/register')
+def register_form():
+    return render_template('admin_register.html')
+
+@app.route('/registermail', methods=['POST'])
+def register_mail():
+    admin_id = request.form.get('adminid')
+    name = request.form.get('name')
+    mail_address = request.form.get('mail')
+    otp = mail.generate_otp()
+    
+    db.insert_admin(admin_id, name, mail_address, otp)
+    
+    to = mail_address
+    subject = 'ワンタイムパスワード'
+    body = f'あなたのワンタイムパスワードは{otp}です。'
+    
+    mail.send_mail(to, subject, body)
+    return redirect(url_for('navigateSend'))
+
+@app.route('/send', methods=['GET'])
+def navigateSend():
+    return render_template('admin_temporary.html')
+
+@app.route('/numbermail', methods=['POST'])
+def send_number():
+    mail_address = request.form.get('mail')
+    authnumber = db.generate_new_number()
+    
+    db.update_number(authnumber, mail_address)
+    
+    to = mail_address
+    subject = '確認番号'
+    body = f'確認番号は{authnumber}です。'
+    
+    mail.send_mail(to, subject, body)
+    if mail_address == '':
+        error = 'このメールアドレスは登録されていません'
+        return render_template('OTP_mail.html', error=error)
+    else:
+        return redirect(url_for('navigateNumber'))
+
+@app.route('/sendnumber', methods=['GET'])
+def navigateNumber():
+    return render_template('confirm_input.html')
+
+@app.route('/confirmnumber', methods=['POST'])
+def confirm_number():
+    pass1 = request.form.get('password1')
+    pass2 = request.form.get('password2')
+    number = request.form.get('number')
+    authnumber = db.number_check(number)
+    
+    if (authnumber and authnumber[6] == number) and pass1 == pass2:
+        db.password_update(pass2, number)
+        msg = 'パスワードを変更しました。'
+        return render_template('index.html', msg=msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 管理者/db.py
+++ 管理者/db.py
@@ -1,4 +1,4 @@
-import os, psycopg2, hashlib, string, random
+import os, psycopg2, hashlib, string, random, mail
 
 def get_connection():
     url = os.environ['DATABASE_URL']
@@ -45,3 +45,105 @@ def login(admin_id, password):
     cursor.close()
     connection.close()
   return flg
+
+def otp_login(admin_id):
+  sql = 'SELECT * FROM admin WHERE id = %s'
+  try:
+    connection=get_connection()
+    cursor=connection.cursor()
+    cursor.execute(sql, (admin_id,))
+    one_time_pass=cursor.fetchone()
+  finally:
+    cursor.close()
+    connection.close()
+  return one_time_pass
+
+def number_check(number):
+  sql = 'SELECT * FROM admin WHERE one_time_pass = %s'
+  try:
+    connection=get_connection()
+    cursor=connection.cursor()
+    cursor.execute(sql, (number,))
+    one_time_pass=cursor.fetchone()
+  finally:
+    cursor.close()
+    connection.close()
+  return one_time_pass
+
+def update_number(number, mail):
+  sql = 'UPDATE admin SET one_time_pass = %s WHERE mail = %s'
+  
+  try :   # 例外処理
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(sql, (number, mail))
+    count = cursor.rowcount # 更新件数を取得
+    connection.commit()
+
+  except psycopg2.DatabaseError:    # Java でいう catch 失敗した時の処理をここに書く
+    count = 0   # 例外が発生したら 0 を return する。
+
+  finally:  # 成功しようが、失敗しようが、close する。
+    cursor.close()
+    connection.close()
+
+  return count
+
+def password_update(password, otp):
+  sql = 'UPDATE admin SET salt = %s, password = %s, tp_flag = True WHERE one_time_pass = %s'
+  salt = get_salt() # ソルトの生成
+  hashed_password = get_hash(password, salt) # 生成したソルトでハッシュ
+  
+  try :   # 例外処理
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(sql, (salt, hashed_password, otp))
+    count = cursor.rowcount # 更新件数を取得
+    connection.commit()
+
+  except psycopg2.DatabaseError:    # Java でいう catch 失敗した時の処理をここに書く
+    count = 0   # 例外が発生したら 0 を return する。
+
+  finally:  # 成功しようが、失敗しようが、close する。
+    cursor.close()
+    connection.close()
+
+  return count
+
+def insert_admin(admin_id, name, mail, otp):
+  sql = 'INSERT INTO admin VALUES (%s, %s, %s, null, null, False, %s)'
+  
+  try :   # 例外処理
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(sql, (admin_id, name, mail, otp))
+    count = cursor.rowcount # 更新件数を取得
+    connection.commit()
+
+  except psycopg2.DatabaseError:    # Java でいう catch 失敗した時の処理をここに書く
+    count = 0   # 例外が発生したら 0 を return する。
+
+  finally:  # 成功しようが、失敗しようが、close する。
+    cursor.close()
+    connection.close()
+
+  return count
+
+# 生成された番号が既にデータベースに存在するか確認する関数
+def number_exists_in_database(number):
+  sql = "SELECT COUNT(*) FROM admin WHERE one_time_pass = %s"
+  connection = get_connection()
+  cursor = connection.cursor()
+  cursor.execute(sql, (number,))
+  count = cursor.fetchone()[0]
+  return count > 0
+
+# 新しい番号を生成する関数
+def generate_new_number():
+    while True:
+        new_number = mail.generate_number()
+        if not number_exists_in_database(new_number):
+            return new_number
